dbb0799dff76d2780aa4941e4e8129ecddb02f54
Joseph_Thairu <joejoement@gmail.com>
2024-04-03 19:58:35 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const submitData = () => {
+    const postData = {
+        name: "Jane Doe",
+        email: "janedoe@yahoo.com"
+    }
+
+    const configureObject = {
+        method: "POST",
+        Headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body:JSON.stringify(postData),
+    }
+    return fetch('http://localhost:3000/users', configureObject)
+    .then(res => res.json())
+    .then(data => {
+        document.body.textContent = data.id;})
+     
+        .catch(error =>{
+            document.body.textContent = error.message
+        })
+}
+
+    
+      
